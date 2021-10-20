e885d3ab644e8cd586db57d735ae7a8836c372d9
realapril <39916556+realapril@users.noreply.github.com>
2021-10-20 15:21:31 +0900

    Changed some names

--- lib/provider/scale_provider.dart
+++ lib/provider/scale_provider.dart
@@ -1,4 +1,4 @@
-// import 'package:crypto_scale_trade/Model/trade_model.dart';
+// import 'package:crypto_scale_trade/model/trade_model.dart';
 // import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
 //
