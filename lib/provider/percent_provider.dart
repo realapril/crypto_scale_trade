fb3fb72f7882a0ab68059b254cc687e2e354a735
realapril <39916556+realapril@users.noreply.github.com>
2021-10-21 01:54:10 +0900

    Made percent calcuationg function, especially ttv_ (Total to value). Made UI, provider both

--- lib/provider/percent_provider.dart
+++ lib/provider/percent_provider.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
 
 class PercentProvider extends ChangeNotifier {
-  double _answerPercentTotal = 0;
 
-  calPercentOfTotal(double whole, double percent){
-    _answerPercentTotal = percent/whole*100;
+  /// Provides 5 kinds of percent calculations
+  ///
+  /// 1. If the value changed from A to B, by what percentage did it increase or Decrease?
+  /// 2. What if the value increased by A percent?
+  /// 3. What if the value decreased by A percent?
+  /// 4. What is A percent of the total value?
+  /// 5. What percentage of the total is A?
+  ///
+  /// 1. vtv_ (Value to value)
+  /// 2. vi_ (Value increase)
+  /// 3. vd_ (Value decrease)
+  /// 4. ttv_ (Total to value)
+  /// 5. vtp_ (Value to percent)
+
+  // 1. vtv_ (Value to value)
+  // 2. vi_ (Value increase)
+  // 3. vd_ (Value decrease)
+
+  // 4. ttv_ (Total to value)
+  double _ttvAnswer = 0.0;
+  double _ttvTotal = 0.0;
+  double _ttvPercent = 0.0;
+
+  setTtvTotal(String total){
+    if(total.isNotEmpty){
+      _ttvTotal = double.parse(total);
+      if(_ttvTotal!=0.0 && _ttvPercent!=0.0){
+        _calTtvAnswer(_ttvTotal, _ttvPercent);
+      }
+    }else{
+      _ttvTotal = 0.0;
+      _resetTtvAnswer();
+    }
   }
-  double get getPercentOfTotal => _answerPercentTotal;
+
+  setTtvPercent(String percent){
+    if(percent.isNotEmpty){
+      _ttvPercent = double.parse(percent);
+      if(_ttvTotal!=0.0 && _ttvPercent!=0.0){
+        _calTtvAnswer(_ttvTotal, _ttvPercent);
+      }
+    }else{
+      _ttvPercent = 0.0;
+      _resetTtvAnswer();
+    }
+  }
+
+  _resetTtvAnswer(){
+    _ttvAnswer = 0;
+    notifyListeners();
+
+  }
+
+  _calTtvAnswer(double total, double percent){//전체의 a 퍼센트는?
+    _ttvAnswer = percent/total*100;
+    print("계산"+ _ttvAnswer.toString());
+    notifyListeners();
+  }
+
+  String get getTtvAnswer => _ttvAnswer.toString();
+
+  // 5. vtp_ (Value to percent)
 }
--- lib/screens/percent_screen.dart
+++ lib/screens/percent_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:crypto_scale_trade/provider/percent_provider.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sticky_headers/sticky_headers.dart';
 
 class PercentScreen extends StatefulWidget{
   @override
@@ -10,10 +15,151 @@ class _PercentScreen extends State<PercentScreen>{
 
   @override
   Widget build(BuildContext context) {
+    PercentProvider percentProvider= Provider.of<PercentProvider>(context);
+
+    TextStyle titleTextStyle = GoogleFonts.lato(
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+    );
+
+    TextStyle planTextStyle = GoogleFonts.lato(
+        fontSize: 18.0,
+        fontWeight: FontWeight.bold,
+        color: Colors.black87,
+    );
+
     return Scaffold(
       appBar: AppBar(title: Text(AppLocalizations.of(context)!.percent)),
-      body: Center(
-        child: Text(AppLocalizations.of(context)!.helloWorld,),
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Column(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    StickyHeader( //4. TTV
+                      header: Container(
+                        height: 50.0,
+                        color: Colors.blueGrey[700],
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        alignment: Alignment.centerLeft,
+                        child: Text('전체의 A퍼센트는 얼마?',
+                          style: titleTextStyle,
+                        ),
+                      ),
+                      content: Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text('전체',
+                                  style: planTextStyle,
+                                ),
+                                const SizedBox(
+                                  width: 85.0,
+                                ),
+                                Expanded(
+                                  child: Container(
+                                      width: 100.0,
+                                      child: TextFormField(
+                                        onChanged: percentProvider.setTtvTotal,
+                                        keyboardType: TextInputType.numberWithOptions(decimal: true),
+                                        textAlign: TextAlign.right,
+                                        decoration: InputDecoration(
+                                          isDense: true,
+                                        ),
+                                      )
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 30.0,
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Row(
+                                children: [
+                                  Text('퍼센트',
+                                    style: planTextStyle,),
+                                  const SizedBox(
+                                    width: 70.0,
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                        width: 100.0,
+                                        child: TextFormField(
+                                          onChanged: percentProvider.setTtvPercent,
+                                          keyboardType: TextInputType.numberWithOptions(decimal: true),
+                                          textAlign: TextAlign.right,
+                                          decoration: InputDecoration(
+                                              isDense: true,
+                                            ),
+                                        )
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 10.0,
+                                  ),
+                                  Text('%',
+                                    style: planTextStyle,),
+                                ],
+                              ),
+                            ),
+                            const Divider(
+                              thickness: 1,
+                              color: Colors.grey,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 4.0),
+                              child: Row(
+                                children: [
+                                  Text('결과',
+                                    style: planTextStyle,),
+                                  const SizedBox(
+                                    width: 84.0,
+                                  ),
+                                  Expanded(
+                                    child: Text(percentProvider.getTtvAnswer,
+                                      style: planTextStyle,
+                                      textAlign: TextAlign.right,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 30.0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                // Row(
+                //   children: [
+                //     Text('증가 퍼센트'),
+                //     TextField(),
+                //     Text('%'),
+                //   ],
+                // ),
+                // Row(
+                //   children: [
+                //     Text('결과'),
+                //     TextField(),
+                //     Text('%'),
+                //   ],
+                // ),
+
+
+              ]
+          ),
+        ),
       ),
     );
   }
