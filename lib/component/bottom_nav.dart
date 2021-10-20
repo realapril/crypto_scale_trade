8bbb2975da34d695b4bbbb02ef662db0377dc0d4
realapril <39916556+realapril@users.noreply.github.com>
2021-10-20 16:10:17 +0900

    Divided BottomNavigationWidget

---
+++ lib/component/bottom_nav.dart
@@ -0,0 +1,68 @@
+import 'package:crypto_scale_trade/provider/bottom_navigation_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class BottomNavigationWidget extends StatelessWidget {
+  const BottomNavigationWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    BottomNavigationProvider bottomNavigationBar = Provider.of<BottomNavigationProvider>(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[850],
+        boxShadow: [
+          BoxShadow(
+            spreadRadius: -10,
+            blurRadius: 60,
+            color: Colors.black.withOpacity(.20),
+            offset: Offset(0, 15),
+          )
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: 16.0, vertical: 8),
+        child: GNav(
+          gap: 10,
+          color: Colors.grey[600],
+          activeColor: Colors.white,
+          rippleColor: Colors.grey[800]!,
+          hoverColor: Colors.grey[700]!,
+          iconSize: 20,
+          textStyle: TextStyle(fontSize: 16, color: Colors.white),
+          tabBackgroundColor: Colors.grey[900]!,
+          padding:
+          EdgeInsets.symmetric(horizontal: 20, vertical: 16.5),
+          duration: Duration(milliseconds: 800),
+          tabs: [
+            GButton(
+              icon: LineIcons.inbox,//searchPlus
+              text: AppLocalizations.of(context)!.saved,
+            ),
+            GButton(
+              icon: LineIcons.calculator,
+              text: AppLocalizations.of(context)!.scale,
+            ),
+            GButton(
+              icon: LineIcons.percent,
+              text: AppLocalizations.of(context)!.percent,
+            ),
+            GButton(
+              icon: LineIcons.cog,
+              text: AppLocalizations.of(context)!.settings,
+            )
+          ],
+          selectedIndex: bottomNavigationBar.getCurrentPage,
+          onTabChange: (index) {
+            bottomNavigationBar.updateCurrentPage(index);
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_scale_trade/component/bottom_nav.dart';
 import 'package:crypto_scale_trade/provider/bottom_navigation_provider.dart';
 import 'package:crypto_scale_trade/screens/percent_screen.dart';
 import 'package:crypto_scale_trade/screens/saved_list_screen.dart';
@@ -9,13 +10,9 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class MainScreen extends StatefulWidget{
-  @override
-  _MainScreen createState()=> _MainScreen();
-}
+class MainScreen extends StatelessWidget{
+  MainScreen({Key? key}) : super(key: key);
 
-class _MainScreen extends State<MainScreen>{
-  //final List<Widget> _screenData = [Screen1(), Screen2(), Screen3(), Screen4()];
   late BottomNavigationProvider bottomNavigationBar;
 
   Widget _navigationBody() {
@@ -39,108 +36,31 @@ class _MainScreen extends State<MainScreen>{
   PreferredSizeWidget _appBar(){
     switch (bottomNavigationBar.getCurrentPage) {
       case 0:
-        return AppBar(title: Text(AppLocalizations.of(context)!.saved));
+        return AppBar(title: Text('Indexed Stack'));
         break;
       case 1:
-        return AppBar(title: Text(AppLocalizations.of(context)!.scale));
+        return AppBar(title: Text('Indexed Stack'));
         break;
       case 2:
-        return AppBar(title: Text(AppLocalizations.of(context)!.percent));
+        return AppBar(title: Text('Indexed Stack'));
         break;
       case 3:
-        return AppBar(title: Text(AppLocalizations.of(context)!.settings));
+        return AppBar(title: Text('Indexed Stack'));
         break;
       default :
-        return AppBar(title: const Text('None'));
+        return AppBar(title: Text(''));
     }
-  }
-
-  Widget _bottomNavigationBarWidget() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey[850],
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: -10,
-            blurRadius: 60,
-            color: Colors.black.withOpacity(.20),
-            offset: Offset(0, 15),
-          )
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-            horizontal: 16.0, vertical: 8),
-        child: GNav(
-          gap: 10,
-          color: Colors.grey[600],
-          activeColor: Colors.white,
-          rippleColor: Colors.grey[800]!,
-          hoverColor: Colors.grey[700]!,
-          iconSize: 20,
-          textStyle: TextStyle(fontSize: 16, color: Colors.white),
-          tabBackgroundColor: Colors.grey[900]!,
-          padding:
-          EdgeInsets.symmetric(horizontal: 20, vertical: 16.5),
-          duration: Duration(milliseconds: 800),
-          tabs: [
-            GButton(
-              icon: LineIcons.inbox,//searchPlus
-              text: AppLocalizations.of(context)!.saved,
-            ),
-            GButton(
-              icon: LineIcons.calculator,
-              text: AppLocalizations.of(context)!.scale,
-            ),
-            GButton(
-              icon: LineIcons.percent,
-              text: AppLocalizations.of(context)!.percent,
-            ),
-            GButton(
-              icon: LineIcons.cog,
-              text: AppLocalizations.of(context)!.settings,
-            )
-          ],
-          selectedIndex: bottomNavigationBar.getCurrentPage,
-          onTabChange: (index) {
-            bottomNavigationBar.updateCurrentPage(index);
-          },
-        ),
-      ),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     bottomNavigationBar = Provider.of<BottomNavigationProvider>(context);
-    int _page = bottomNavigationBar.getCurrentPage;
-
-
 
     return Scaffold(
       appBar: _appBar(),
       body: _navigationBody(),
-      bottomNavigationBar: _bottomNavigationBarWidget(),
+      bottomNavigationBar: const BottomNavigationWidget(),
     );
   }
-
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return WillPopScope(
-  //     onWillPop: _onWillPop,
-  //     child: Scaffold(
-  //       appBar: AppBar(
-  //         elevation: 20,
-  //         title: const Text('percent'),
-  //         automaticallyImplyLeading: false,
-  //       ),
-  //       body: Center(
-  //         child: Text(AppLocalizations.of(context)!.helloWorld,),
-  //       ),
-  //       bottomNavigationBar: const BottomGNav(),
-  //     ),
-  //   );
-  // }
 
 }
