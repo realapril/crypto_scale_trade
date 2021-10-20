87db57b7277233a428aa865604c08c4fabfabcfd
realapril <39916556+realapril@users.noreply.github.com>
2021-10-21 04:44:36 +0900

    Made all percent providers

--- lib/provider/percent_provider.dart
+++ lib/provider/percent_provider.dart
@@ -58,7 +58,84 @@ class PercentProvider extends ChangeNotifier {
 
 
   // 2. vi_ (Value increase)
+  double _viTotal =0.0;
+  double _viPercent =0.0;
+  double _viAnswer =0.0;
+
+  String get getViAnswer => _viAnswer.toString();
+
+  setViTotal(String total){
+    if(total.isNotEmpty){
+      _viTotal = double.parse(total);
+      if(_viTotal!=0.0 && _viPercent!=0.0){
+        _calViAnswer(_viTotal, _viPercent);
+      }
+    }else{
+      _viTotal = 0.0;
+      _resetViAnswer();
+    }
+  }
+  setViPercent(String percent){
+    if(percent.isNotEmpty){
+      _viPercent = double.parse(percent);
+      if(_viTotal!=0.0 && _viPercent!=0.0){
+        _calViAnswer(_viTotal, _viPercent);
+      }
+    }else{
+      _viPercent = 0.0;
+      _resetViAnswer();
+    }
+  }
+
+  _resetViAnswer(){
+    _viAnswer = 0;
+    notifyListeners();
+  }
+
+  _calViAnswer(double total, double percent){
+    _viAnswer = total * (1+ percent/100);
+    notifyListeners();
+  }
+
   // 3. vd_ (Value decrease)
+  double _vdTotal =0.0;
+  double _vdPercent =0.0;
+  double _vdAnswer =0.0;
+
+  String get getVdAnswer => _vdAnswer.toString();
+
+  setVdTotal(String total){
+    if(total.isNotEmpty){
+      _vdTotal = double.parse(total);
+      if(_vdTotal!=0.0 && _vdPercent!=0.0){
+        _calVdAnswer(_vdTotal, _vdPercent);
+      }
+    }else{
+      _vdTotal = 0.0;
+      _resetVdAnswer();
+    }
+  }
+  setVdPercent(String percent){
+    if(percent.isNotEmpty){
+      _vdPercent = double.parse(percent);
+      if(_vdTotal!=0.0 && _vdPercent!=0.0){
+        _calVdAnswer(_vdTotal, _vdPercent);
+      }
+    }else{
+      _vdPercent = 0.0;
+      _resetVdAnswer();
+    }
+  }
+
+  _resetVdAnswer(){
+    _vdAnswer = 0;
+    notifyListeners();
+  }
+
+  _calVdAnswer(double total, double percent){
+    _vdAnswer = total * percent/100;
+    notifyListeners();
+  }
 
   // 4. ttv_ (Total to value)
   double _ttvAnswer = 0.0;
@@ -96,11 +173,51 @@ class PercentProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  _calTtvAnswer(double total, double percent){//전체의 a 퍼센트는?
+  _calTtvAnswer(double total, double percent){
     _ttvAnswer = percent/total*100;
     notifyListeners();
   }
 
 
   // 5. vtp_ (Value to percent)
+  double _vtpTotal =0.0;
+  double _vtpValue =0.0;
+  double _vtpPercent =0.0;
+
+  String get getVtpPercent => _vtpPercent.toString();
+
+  setVtpA(String total){
+    if(total.isNotEmpty){
+      _vtpTotal = double.parse(total);
+      if(_vtpTotal!=0.0 && _vtpValue!=0.0){
+        _calVtpPercent(_vtpTotal, _vtpValue);
+      }
+    }else{
+      _vtpTotal = 0.0;
+      _resetVtpPercent();
+    }
+  }
+
+  setVtpB(String value){
+    if(value.isNotEmpty){
+      _vtpValue = double.parse(value);
+      if(_vtpTotal!=0.0 && _vtpValue!=0.0){
+        _calVtpPercent(_vtpTotal, _vtpValue);
+      }
+    }else{
+      _vtpTotal = 0.0;
+      _resetVtpPercent();
+    }
+  }
+  _resetVtpPercent(){
+    _vtpPercent = 0;
+    notifyListeners();
+  }
+
+  _calVtpPercent(double total, double value){
+    _vtpPercent = value/total*100;
+    notifyListeners();
+  }
+
+
 }
