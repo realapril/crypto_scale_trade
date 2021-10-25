f9912f165158832afeb0da9c3ecfeccb0efc2ff5
realapril <39916556+realapril@users.noreply.github.com>
2021-10-26 03:59:21 +0900

    Added background and icon to dismissable

--- lib/component/plan_list_view.dart
+++ lib/component/plan_list_view.dart
@@ -4,6 +4,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:line_icons/line_icons.dart';
+
+
 class PlanListView extends StatefulWidget {
   int index=0;
   List<BuyingPlan> planList;
@@ -35,98 +38,109 @@ class _PlanListViewState extends State<PlanListView> {
         planProvider.updatePlanAmount(widget.index, widget.planList[widget.index].myController2.text);
       }
     });
-    return Padding(
-        padding: const EdgeInsets.fromLTRB(12, 10, 12, 12),
-        child: Dismissible(
-          key: Key(counter.toString()),
-          direction: DismissDirection.startToEnd,
-          onDismissed: (direction){
-            planProvider.deletePlan(widget.index);
-          },
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Text('평단')
-                  ),
-                  const Expanded(
-                    flex: 1,
-                    child: SizedBox(
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: TextField(
-                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                      controller: widget.planList[widget.index].myController1,
-                      textInputAction: TextInputAction.go,
-                      textAlign: TextAlign.right,
-                      decoration: const InputDecoration(
-                        isDense: true,
-                      ),
-                    ),
-                  ),
-                  Text('원'),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Text('수량')
-                  ),
-                  const Expanded(
-                    flex: 1,
-                    child: SizedBox(
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: TextField(
-                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                      controller: widget.planList[widget.index].myController2,
-                      textInputAction: TextInputAction.go,
-                      textAlign: TextAlign.right,
-                      decoration: const InputDecoration(
-                        isDense: true,
-                      ),
-                    ),
-                  ),
-                  Text('개')
-                ],
-              ),
-              SizedBox(height: 10,),
-              Row(
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Text('투자금액')
-                  ),
-                  const Expanded(
-                    flex: 1,
-                    child: SizedBox(
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child:
-                    Text(widget.planList[widget.index].totalValue,
-                      textAlign: TextAlign.right,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  Text('원'),
-                ],
-              ),
-              // Divider(
-              //   color: Colors.black54,
-              // )
-            ],
+    final Widget secondaryBackground;
+    return Dismissible(
+      key: Key(counter.toString()),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction){
+        planProvider.deletePlan(widget.index);
+      },
+      background: Container (
+        color: Colors.red,
+        //Use ListTile here because you can quickly set up ends of the Icon
+        child: const ListTile (
+          leading: Icon (
+            Icons.delete,
+            color: Colors.white,
           ),
         ),
-      );
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(12, 10, 12, 12),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Expanded(
+                    flex: 1,
+                    child: Text('평단')
+                ),
+                const Expanded(
+                  flex: 1,
+                  child: SizedBox(
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: TextField(
+                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                    controller: widget.planList[widget.index].myController1,
+                    textInputAction: TextInputAction.go,
+                    textAlign: TextAlign.right,
+                    decoration: const InputDecoration(
+                      isDense: true,
+                    ),
+                  ),
+                ),
+                Text('원'),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Text('수량')
+                ),
+                const Expanded(
+                  flex: 1,
+                  child: SizedBox(
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: TextField(
+                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                    controller: widget.planList[widget.index].myController2,
+                    textInputAction: TextInputAction.go,
+                    textAlign: TextAlign.right,
+                    decoration: const InputDecoration(
+                      isDense: true,
+                    ),
+                  ),
+                ),
+                Text('개')
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Text('투자금액')
+                ),
+                const Expanded(
+                  flex: 1,
+                  child: SizedBox(
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child:
+                  Text(widget.planList[widget.index].totalValue,
+                    textAlign: TextAlign.right,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Text('원'),
+              ],
+            ),
+            // Divider(
+            //   color: Colors.black54,
+            // )
+          ],
+        ),
+      ),
+    );
   }
 
 
